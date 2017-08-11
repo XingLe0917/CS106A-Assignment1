9eec6463349c24494e5c32bd01015829236452e4
xinle <xingle@sportsdata.cn>
2017-08-11 16:16:39 +0800

    add files

---
+++ src/StoneMasonKarel.java
@@ -0,0 +1,18 @@
+/*
+ * TODO: comment this program
+ */
+
+import stanford.karel.*;
+
+public class StoneMasonKarel extends SuperKarel {
+	
+	// TODO: write the code to implement this program
+	public void run() {
+		move();
+		turnLeft();
+		move();
+		move();
+	}
+
+
+}
