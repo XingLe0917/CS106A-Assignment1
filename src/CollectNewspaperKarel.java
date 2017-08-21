6a67b8909085b51ce80b74588ba6733e153f0792
xinle <xingle@sportsdata.cn>
2017-08-21 18:00:12 +0800

    add assignment1 code

--- src/CollectNewspaperKarel.java
+++ src/CollectNewspaperKarel.java
@@ -7,5 +7,65 @@ import stanford.karel.*;
 public class CollectNewspaperKarel extends SuperKarel {
 	
 	// TODO: write the code to implement this program
+	
+	/*
+	 * The problem you need to get Karel to solve is to collect the 
+	 * newspaper—represented (as all objects in Karel’s world are) by a beeper—from outside the doorway 
+	 * and then to return to its initial position, facing east.
+	 */
+	
+	public void run(){
+//		moveToNewspaper();
+		moveToOutside();
+		pickBeeper();
+		moveToInitail();	
+	}
+	
+	/*
+	 * 移动到门外
+	 */
+	private void moveToOutside() {
+		while(leftIsBlocked()){
+			if(frontIsClear()){
+				move();
+			}
+			else{
+				turnRight();
+			}
+		}
+		turnLeft();
+		move();
+	}
+	
+	/*
+	 * 走到要收集的报纸位置
+	 */
+	private void moveToNewspaper() {
+		moveToWall();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+	}
+	
+	/*
+	 * 移动到原始位置
+	 */
+	private void moveToInitail() {
+		turnRight();
+		moveToWall();
+		turnRight();
+		moveToWall();
+		turnAround();
+	}
+	
+	/*
+	 * 向前走到墙
+	 */
+	private void moveToWall() {
+		while(frontIsClear()){
+			move();
+		}
+	}
 
 }
--- src/StoneMasonKarel.java
+++ src/StoneMasonKarel.java
@@ -6,11 +6,44 @@ import stanford.karel.*;
 
 public class StoneMasonKarel extends SuperKarel {
 	
-	// TODO: write the code to implement this program
+	// TODO: 修复损坏的拱桥
 	public void run() {
-		move();
+		boolean flag = true;
+		while(flag){
+			putBeeperInLine();
+			for(int i= 0;i<4;i++){
+				if(frontIsClear()){
+					move();
+				}
+				else{
+					flag = false;
+					break;
+				}
+			}
+		}
+	}
+		
+	/*
+	 * 向上放置一竖排的石块，然后回到最下方位置
+	 */
+	private void putBeeperInLine() {
+		turnLeft();
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+		}
+		if(frontIsBlocked()){
+			turnAround();
+			while(frontIsClear()){
+				move();
+			}
+		}
 		turnLeft();
 	}
-
 
 }
